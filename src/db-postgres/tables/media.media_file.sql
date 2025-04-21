58b3c907b1c7e17c44986cfdac706dd24d3baa68
Mike Morano <mmorano@mikeandwan.us>
2025-04-21 09:07:31 -0400

    lets go back to composite key

--- src/db-postgres/tables/media.media_file.sql
+++ src/db-postgres/tables/media.media_file.sql
@@ -1,5 +1,4 @@
 CREATE TABLE IF NOT EXISTS media.media_file (
-    id UUID NOT NULL,
     media_id UUID NOT NULL,
     media_type_id UUID NOT NULL,
     scale_id UUID NOT NULL,
@@ -9,7 +8,7 @@ CREATE TABLE IF NOT EXISTS media.media_file (
     path TEXT,
 
     CONSTRAINT pk_media_media_file
-    PRIMARY KEY (id),
+    PRIMARY KEY (media_id, media_type_id, scale_id),
 
     CONSTRAINT fk_media_media_file$media_media
     FOREIGN KEY (media_id)
@@ -23,26 +22,6 @@ CREATE TABLE IF NOT EXISTS media.media_file (
     FOREIGN KEY (media_type_id)
     REFERENCES media.media_type(id)
 );
-
-DO
-$$
-BEGIN
-    IF NOT EXISTS
-    (
-        SELECT 1
-        FROM pg_catalog.pg_indexes
-        WHERE schemaname = 'media'
-            AND tablename = 'media_file'
-            AND indexname = 'ix_media_file$media_id$media_type_id$scale_id'
-    )
-    THEN
-
-        CREATE INDEX ix_media_file$media_id$media_type_id$scale_id
-        ON media.media_file(media_id, media_type_id, scale_id);
-
-    END IF;
-END
-$$;
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON media.media_file
