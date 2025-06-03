6530396f4f672d3d4c30da77c802fc5e045a8fe1
Mike Morano <mmorano@mikeandwan.us>
2025-06-03 19:56:30 -0400

    edits to try and bring these in line

--- src/db-postgres/tables/media.role.sql
+++ src/db-postgres/tables/media.role.sql
@@ -1,12 +1,18 @@
 CREATE TABLE IF NOT EXISTS media.role (
     id UUID NOT NULL,
     name TEXT NOT NULL,
+    created TIMESTAMPTZ NOT NULL,
+    created_by UUID NOT NULL,
 
     CONSTRAINT pk_media_role
     PRIMARY KEY (id),
 
     CONSTRAINT uq_media_role$name
-    UNIQUE (name)
+    UNIQUE (name),
+
+    CONSTRAINT fk_media_role$media_user
+    FOREIGN KEY (created_by)
+    REFERENCES media.user(id)
 );
 
 GRANT SELECT, INSERT, UPDATE, DELETE
