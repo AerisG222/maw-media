ff6307d9421422a8f0ba797e4095f6421734e33e
Mike Morano <mmorano@mikeandwan.us>
2025-06-04 18:34:38 -0400

    lets have the media_type capture if it is the poster for now...

--- src/db-postgres/seed/media.media_type.sql
+++ src/db-postgres/seed/media.media_type.sql
@@ -6,6 +6,7 @@ BEGIN
 
         INSERT INTO media.media_type (id, name) VALUES ('01964f94-fa50-7846-b2e6-26d4609cc972', 'photo');
         INSERT INTO media.media_type (id, name) VALUES ('01964f94-fa51-705b-b0e2-b4c668ac6fab', 'video');
+        INSERT INTO media.media_type (id, name) VALUES ('01964f94-fa51-705b-b0e2-b4c668ac6fcd', 'video-poster');
 
     END IF;
 
