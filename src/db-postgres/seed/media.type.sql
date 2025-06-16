87089ac1a8604ca8dec8826f9a0f40aaaeeeffa3
Mike Morano <mmorano@mikeandwan.us>
2025-06-16 17:27:55 -0400

    correct column name

--- src/db-postgres/seed/media.type.sql
+++ src/db-postgres/seed/media.type.sql
@@ -4,9 +4,9 @@ BEGIN
 
     IF NOT EXISTS (SELECT 1 FROM media.type) THEN
 
-        INSERT INTO media.type (id, name) VALUES ('01964f94-fa50-7846-b2e6-26d4609cc972', 'photo');
-        INSERT INTO media.type (id, name) VALUES ('01964f94-fa51-705b-b0e2-b4c668ac6fab', 'video');
-        INSERT INTO media.type (id, name) VALUES ('01964f94-fa51-705b-b0e2-b4c668ac6fcd', 'video-poster');
+        INSERT INTO media.type (id, code) VALUES ('01964f94-fa50-7846-b2e6-26d4609cc972', 'photo');
+        INSERT INTO media.type (id, code) VALUES ('01964f94-fa51-705b-b0e2-b4c668ac6fab', 'video');
+        INSERT INTO media.type (id, code) VALUES ('01964f94-fa51-705b-b0e2-b4c668ac6fcd', 'video-poster');
 
     END IF;
 
