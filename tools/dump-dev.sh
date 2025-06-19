8ad5b2e5533396d5068a1556380e3cf19f2fad42
Mike Morano <mmorano@mikeandwan.us>
2025-06-18 20:23:56 -0400

    add script to easily dump the dev db

--- tools/dump-dev.sh
+++ tools/dump-dev.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-PSQLUSER=svc_maw_media
+PSQLUSER=postgres
 DB=maw_media
 
 podman run -it --rm \
     --pod dev-media-pod \
-    --name dev-media-pg-query \
+    --name dev-media-pg-dump \
     --env "POSTGRES_PASSWORD_FILE=/secrets/psql-${PSQLUSER}" \
     --volume "/home/mmorano/maw/dev/media/data/pgpwd:/secrets" \
     docker.io/library/postgres:17 \
-        psql \
+        pg_dump \
         -h localhost \
         -U "${PSQLUSER}" \
         -d "${DB}"
