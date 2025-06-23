6b7ad4de1c4f034cdff69b28a27cb27e5808d2f5
Mike Morano <mmorano@mikeandwan.us>
2025-06-23 09:03:38 -0400

    prepare a test project to exercise our db / repo code

---
+++ tests/MawMedia.Services.Tests/CategoryRepositoryTests.cs
@@ -0,0 +1,10 @@
+﻿namespace MawMedia.Services.Tests;
+
+public class CategoryRepositoryTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
---
+++ tests/MawMedia.Services.Tests/MediaRepositoryTests.cs
@@ -0,0 +1,10 @@
+﻿namespace MawMedia.Services.Tests;
+
+public class MediaRepositoryTests
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(true);
+    }
+}
