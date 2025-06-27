69b78be285f7654787a0549eb83475017e99aa93
Mike Morano <mmorano@mikeandwan.us>
2025-06-27 07:40:47 -0400

    seed some additional tables (w/ initial help from ai)

--- tests/MawMedia.Services.Tests/Constants.cs
+++ tests/MawMedia.Services.Tests/Constants.cs
@@ -22,4 +22,30 @@ public static class Constants
             role_id = ROLE_FRIEND
         }
     ];
+
+    public static readonly Guid CATEGORY_NATURE = Guid.CreateVersion7();
+    public static readonly Guid CATEGORY_TRAVEL = Guid.CreateVersion7();
+
+    public static readonly Guid MEDIA_NATURE_1 = Guid.CreateVersion7();
+    public static readonly Guid MEDIA_TRAVEL_1 = Guid.CreateVersion7();
+
+    public static readonly Guid LOCATION_NY = Guid.CreateVersion7();
+
+    // ** from db seed scripts **
+    public static readonly Guid TYPE_PHOTO = Guid.Parse("01964f94-fa50-7846-b2e6-26d4609cc972");
+    public static readonly Guid TYPE_VIDEO = Guid.Parse("01964f94-fa51-705b-b0e2-b4c668ac6fab");
+    public static readonly Guid TYPE_VIDEO_POSTER = Guid.Parse("01964f94-fa51-705b-b0e2-b4c668ac6fcd");
+
+    public static readonly Guid SCALE_QQVG = Guid.Parse("01965306-0786-7af9-b3eb-a4dd6ef83505");
+    public static readonly Guid SCALE_QQVG_FILL = Guid.Parse("01965306-0786-7af9-b3eb-a4dd6ef83606");
+    public static readonly Guid SCALE_QVG = Guid.Parse("01965306-49b3-7212-b55d-345e7195a3b0");
+    public static readonly Guid SCALE_QVG_FILL = Guid.Parse("01965306-49b3-7212-b55d-345e7195a6b6");
+    public static readonly Guid SCALE_NHD = Guid.Parse("01965306-6f04-739f-aea6-3b4022f1d2ce");
+    public static readonly Guid SCALE_NHD_FILL = Guid.Parse("01965306-6f04-739f-aea6-3b4022f1d6c6");
+    public static readonly Guid SCALE_FULL_HD = Guid.Parse("01965306-9387-7cb7-8945-1d626de296fa");
+    public static readonly Guid SCALE_QHD = Guid.Parse("01965306-b398-70e8-9bd4-af9e0bb96c8b");
+    public static readonly Guid SCALE_4K = Guid.Parse("01965306-d3b1-754a-abf1-be97f6b18a83");
+    public static readonly Guid SCALE_5K = Guid.Parse("01965307-039f-732f-a768-c09584310119");
+    public static readonly Guid SCALE_8K = Guid.Parse("01965307-20f9-7e01-955e-be53d178662d");
+    public static readonly Guid SCALE_SRC = Guid.Parse("01965307-20f9-7e01-955e-be53d1786828");
 }
--- tests/MawMedia.Services.Tests/DatabaseSeeder.cs
+++ tests/MawMedia.Services.Tests/DatabaseSeeder.cs
@@ -1,5 +1,7 @@
+using System.Text.Json;
 using Dapper;
 using Npgsql;
+using NpgsqlTypes;
 
 namespace MawMedia.Services.Tests;
 
@@ -58,6 +60,13 @@ public class DatabaseSeeder
         await PopulateUsers(conn);
         await PopulateRoles(conn);
         await PopulateUserRoles(conn);
+
+        await PopulateLocations(conn);
+        await PopulateCategories(conn);
+        await PopulateMedia(conn);
+        await PopulateFiles(conn);
+        await PopulateComments(conn);
+        await PopulateFavorites(conn);
     }
 
     async Task PopulateRoles(NpgsqlConnection conn)
@@ -140,6 +149,260 @@ public class DatabaseSeeder
             (@user_id, @role_id, @created, @created_by)
             """,
             userRoles
+        );
+    }
+
+    async Task PopulateCategories(NpgsqlConnection conn)
+    {
+        List<object> categories = [
+            new {
+                id = Constants.CATEGORY_NATURE,
+                name = "Nature",
+                effective_date = DateTime.UtcNow.Date,
+                created = DateTime.UtcNow,
+                created_by = Constants.USER_ADMIN,
+                modified = DateTime.UtcNow,
+                modified_by = Constants.USER_ADMIN
+            },
+            new {
+                id = Constants.CATEGORY_TRAVEL,
+                name = "Travel",
+                effective_date = DateTime.UtcNow.Date,
+                created = DateTime.UtcNow,
+                created_by = Constants.USER_ADMIN,
+                modified = DateTime.UtcNow,
+                modified_by = Constants.USER_ADMIN
+            }
+        ];
+
+        await conn.ExecuteAsync(
+            """
+            INSERT INTO media.category (id, name, effective_date, created, created_by, modified, modified_by)
+            VALUES
+            (@id, @name, @effective_date, @created, @created_by, @modified, @modified_by);
+            """,
+            categories
+        );
+    }
+
+    async Task PopulateCategoryRoles(NpgsqlConnection conn)
+    {
+        List<object> categoryRoles = [
+            new {
+                category_id = Constants.CATEGORY_NATURE,
+                role_id = Constants.ROLE_ADMIN,
+                created = DateTime.UtcNow,
+                created_by = Constants.USER_ADMIN
+            }
+        ];
+
+        await conn.ExecuteAsync(
+            """
+            INSERT INTO media.category_role (category_id, role_id, created, created_by)
+            VALUES
+            (@category_id, @role_id, @created, @created_by);
+            """,
+            categoryRoles
+        );
+    }
+
+    async Task PopulateCategoryFavorites(NpgsqlConnection conn)
+    {
+        List<object> favorites = [
+            new {
+                created_by = Constants.USER_JOHNDOE,
+                category_id = Constants.CATEGORY_NATURE,
+                created = DateTime.UtcNow
+            }
+        ];
+
+        await conn.ExecuteAsync(
+            """
+            INSERT INTO media.category_favorite (created_by, category_id, created)
+            VALUES
+            (@created_by, @category_id, @created);
+            """,
+            favorites
+        );
+    }
+
+    async Task PopulateMedia(NpgsqlConnection conn)
+    {
+        List<dynamic> media = [
+            new {
+                id = Constants.MEDIA_NATURE_1,
+                type_id = Constants.TYPE_PHOTO,
+                location_id = Constants.LOCATION_NY,
+                location_override_id = DBNull.Value,
+                created = DateTime.UtcNow,
+                created_by = Constants.USER_ADMIN,
+                modified = DateTime.UtcNow,
+                modified_by = Constants.USER_ADMIN,
+                metadata = JsonDocument.Parse("{}")
+            }
+        ];
+
+        foreach (var m in media)
+        {
+            await using var cmd = new NpgsqlCommand(
+                """
+                INSERT INTO media.media (id, type_id, location_id, location_override_id, created, created_by, modified, modified_by, metadata)
+                VALUES
+                ($1, $2, $3, $4, $5, $6, $7, $8, $9);
+                """,
+                conn)
+            {
+                Parameters = {
+                    new() { Value = m.id },
+                    new() { Value = m.type_id },
+                    new() { Value = m.location_id },
+                    new() { Value = m.location_override_id, NpgsqlDbType = NpgsqlDbType.Uuid },
+                    new() { Value = m.created },
+                    new() { Value = m.created_by },
+                    new() { Value = m.modified },
+                    new() { Value = m.modified_by },
+                    new() { Value = m.metadata }
+                }
+            };
+
+            await cmd.ExecuteNonQueryAsync();
+        }
+    }
+
+    async Task PopulateCategoryMedia(NpgsqlConnection conn)
+    {
+        List<object> categoryMedia = [
+            new {
+                category_id = Constants.CATEGORY_NATURE,
+                media_id = Constants.MEDIA_NATURE_1,
+                is_teaser = true,
+                created = DateTime.UtcNow,
+                created_by = Constants.USER_ADMIN,
+                modified = DateTime.UtcNow,
+                modified_by = Constants.USER_ADMIN
+            }
+        ];
+
+        await conn.ExecuteAsync(
+            """
+            INSERT INTO media.category_media (category_id, media_id, is_teaser, created, created_by, modified, modified_by)
+            VALUES
+            (@category_id, @media_id, @is_teaser, @created, @created_by, @modified, @modified_by);
+            """,
+            categoryMedia
+        );
+    }
+
+    async Task PopulateFiles(NpgsqlConnection conn)
+    {
+        List<object> files = [
+            new {
+                media_id = Constants.MEDIA_NATURE_1,
+                type_id = Constants.TYPE_PHOTO,
+                scale_id = Constants.SCALE_FULL_HD,
+                width = 1920,
+                height = 1080,
+                bytes = 123456L,
+                path = "/media/nature1.jpg"
+            }
+        ];
+
+        await conn.ExecuteAsync(
+            """
+            INSERT INTO media.file (media_id, type_id, scale_id, width, height, bytes, path)
+            VALUES
+            (@media_id, @type_id, @scale_id, @width, @height, @bytes, @path);
+            """,
+            files
+        );
+    }
+
+    async Task PopulateComments(NpgsqlConnection conn)
+    {
+        List<object> comments = [
+            new {
+                id = Guid.NewGuid(),
+                media_id = Constants.MEDIA_NATURE_1,
+                created = DateTime.UtcNow,
+                created_by = Constants.USER_JOHNDOE,
+                modified = DateTime.UtcNow,
+                body = "Great photo!"
+            }
+        ];
+
+        await conn.ExecuteAsync(
+            """
+            INSERT INTO media.comment (id, media_id, created, created_by, modified, body)
+            VALUES
+            (@id, @media_id, @created, @created_by, @modified, @body);
+            """,
+            comments
+        );
+    }
+
+    async Task PopulateFavorites(NpgsqlConnection conn)
+    {
+        List<object> favorites = [
+            new {
+                media_id = Constants.MEDIA_NATURE_1,
+                created_by = Constants.USER_JOHNDOE,
+                created = DateTime.UtcNow
+            }
+        ];
+
+        await conn.ExecuteAsync(
+            """
+            INSERT INTO media.favorite (media_id, created_by, created)
+            VALUES
+            (@media_id, @created_by, @created);
+            """,
+            favorites
+        );
+    }
+
+    async Task PopulateLocations(NpgsqlConnection conn)
+    {
+        List<object> locations = [
+            new {
+                id = Constants.LOCATION_NY,
+                latitude = 40.712776m,
+                longitude = -74.005974m,
+                lookup_date = DateTime.UtcNow,
+                formatted_address = "New York, NY, USA",
+                administrative_area_level_1 = "NY",
+                administrative_area_level_2 = "New York County",
+                administrative_area_level_3 = (string?)null,
+                country = "USA",
+                locality = "New York",
+                neighborhood = "Manhattan",
+                sub_locality_level_1 = (string?)null,
+                sub_locality_level_2 = (string?)null,
+                postal_code = "10007",
+                postal_code_suffix = (string?)null,
+                premise = (string?)null,
+                route = "Broadway",
+                street_number = "1",
+                sub_premise = (string?)null
+            }
+        ];
+
+        await conn.ExecuteAsync(
+            """
+            INSERT INTO media.location (
+                id, latitude, longitude, lookup_date, formatted_address,
+                administrative_area_level_1, administrative_area_level_2, administrative_area_level_3,
+                country, locality, neighborhood, sub_locality_level_1, sub_locality_level_2,
+                postal_code, postal_code_suffix, premise, route, street_number, sub_premise
+            )
+            VALUES
+            (
+                @id, @latitude, @longitude, @lookup_date, @formatted_address,
+                @administrative_area_level_1, @administrative_area_level_2, @administrative_area_level_3,
+                @country, @locality, @neighborhood, @sub_locality_level_1, @sub_locality_level_2,
+                @postal_code, @postal_code_suffix, @premise, @route, @street_number, @sub_premise
+            );
+            """,
+            locations
         );
     }
 }
