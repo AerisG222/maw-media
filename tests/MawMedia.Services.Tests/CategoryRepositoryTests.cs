4e06331cff9f22a4e9c3ea0a1216bf2d7918e585
Mike Morano <mmorano@mikeandwan.us>
2025-06-27 20:33:02 -0400

    start to convert similar tests to theories

--- tests/MawMedia.Services.Tests/CategoryRepositoryTests.cs
+++ tests/MawMedia.Services.Tests/CategoryRepositoryTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging.Testing;
+﻿using MawMedia.Services.Tests.Models;
+using Microsoft.Extensions.Logging.Testing;
 
 namespace MawMedia.Services.Tests;
 
@@ -13,60 +14,54 @@ public class CategoryRepositoryTests
         _fixture = fixture;
     }
 
-    [Fact]
-    public async Task GetCategory_InvalidUserAndCategory_ReturnsNull()
+    public static TheoryData<Guid, int> GetYearsData => new()
+    {
+        { Guid.CreateVersion7(),  0 },
+        { Constants.USER_ADMIN,   2 },
+        { Constants.USER_JOHNDOE, 1 }
+    };
+
+    [Theory]
+    [MemberData(nameof(GetYearsData))]
+    public async Task GetYears(Guid userId, int expectedCount)
     {
         var repo = GetRepo();
 
-        var cat = await repo.GetCategory(Guid.CreateVersion7(), Guid.CreateVersion7());
+        var years = await repo.GetCategoryYears(userId);
 
-        Assert.Null(cat);
+        Assert.NotNull(years);
+        Assert.Equal(expectedCount, years.Count());
     }
 
-    [Fact]
-    public async Task GetCategory_InvalidUser_ReturnsNull()
+    public static TheoryData<Guid, Guid, DbCategory?> GetCategoryData => new()
+    {
+        { Guid.CreateVersion7(),  Guid.CreateVersion7(),        null },
+        { Guid.CreateVersion7(),  Constants.CATEGORY_NATURE.Id, null },
+        { Constants.USER_ADMIN,   Guid.CreateVersion7(),        null },
+        { Constants.USER_ADMIN,   Constants.CATEGORY_NATURE.Id, Constants.CATEGORY_NATURE },
+        { Constants.USER_JOHNDOE, Constants.CATEGORY_NATURE.Id, null }
+    };
+
+    [Theory]
+    [MemberData(nameof(GetCategoryData))]
+    public async Task GetCategory(Guid userId, Guid categoryId, DbCategory? expected)
     {
         var repo = GetRepo();
 
-        var cat = await repo.GetCategory(Guid.CreateVersion7(), Constants.CATEGORY_NATURE.Id);
+        var cat = await repo.GetCategory(userId, categoryId);
 
-        Assert.Null(cat);
-    }
-
-    [Fact]
-    public async Task GetCategory_InvalidCategory_ReturnsNull()
-    {
-        var repo = GetRepo();
-
-        var cat = await repo.GetCategory(Constants.USER_ADMIN, Guid.CreateVersion7());
-
-        Assert.Null(cat);
-    }
-
-    [Fact]
-    public async Task GetCategory_WhenUserHasAccess_ReturnsCategory()
-    {
-        var repo = GetRepo();
-
-        var cat = await repo.GetCategory(Constants.USER_ADMIN, Constants.CATEGORY_NATURE.Id);
-
-        Assert.NotNull(cat);
-        Assert.Equal(Constants.CATEGORY_NATURE.Id, cat.Id);
-        Assert.Equal(Constants.CATEGORY_NATURE.Name, cat.Name);
-        Assert.Equal(Constants.CATEGORY_NATURE.EffectiveDate, cat.EffectiveDate);
-
-        // microsecond precision can be lost w/ nodatime, which is fine for us - use tostring as proxy for equality
-        Assert.Equal(Constants.CATEGORY_NATURE.Modified.ToString(), cat.Modified.ToString());
-    }
-
-    [Fact]
-    public async Task GetCategory_WhenUserDoesNotHaveAccess_ReturnsNull()
-    {
-        var repo = GetRepo();
-
-        var cat = await repo.GetCategory(Constants.USER_JOHNDOE, Constants.CATEGORY_NATURE.Id);
-
-        Assert.Null(cat);
+        if (expected == null)
+        {
+            Assert.Null(cat);
+        }
+        else
+        {
+            Assert.NotNull(cat);
+            Assert.Equal(expected.Id, cat.Id);
+            Assert.Equal(expected.Name, cat.Name);
+            Assert.Equal(expected.EffectiveDate, cat.EffectiveDate);
+            Assert.Equal(expected.Modified.ToString(), cat.Modified.ToString());
+        }
     }
 
     [Fact]
@@ -101,29 +96,6 @@ public class CategoryRepositoryTests
         Assert.NotNull(cats);
         Assert.Single(cats);
         Assert.Equal(Constants.CATEGORY_TRAVEL.Id, cats.First().Id);
-    }
-
-    [Fact]
-    public async Task GetYears_ForAdmin_ReturnsBoth()
-    {
-        var repo = GetRepo();
-
-        var cats = await repo.GetCategoryYears(Constants.USER_ADMIN);
-
-        Assert.NotNull(cats);
-        Assert.Equal(2, cats.Count());
-    }
-
-    [Fact]
-    public async Task GetYears_ForRestrictedUser_ReturnsOne()
-    {
-        var repo = GetRepo();
-
-        var cats = await repo.GetCategoryYears(Constants.USER_JOHNDOE);
-
-        Assert.NotNull(cats);
-        Assert.Single(cats);
-        Assert.Equal(Constants.CATEGORY_TRAVEL.EffectiveDate.Year, cats.First());
     }
 
     [Fact]
