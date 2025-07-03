a01c1631f6722dc3552a3f821e4e7c50bf189752
Mike Morano <mmorano@mikeandwan.us>
2025-07-03 16:46:33 -0400

    include roles which are at server/cluster level

--- tools/dev-dump.sh
+++ tools/dev-dump.sh
@@ -4,6 +4,18 @@ DB=maw_media
 
 podman run --rm \
     --pod dev-media-pod \
+    --name dev-media-pg-dumpall \
+    --env "POSTGRES_PASSWORD_FILE=/secrets/psql-${PSQLUSER}" \
+    --volume "/home/mmorano/maw/dev/media/data/pgpwd:/secrets" \
+    docker.io/library/postgres:17 \
+        pg_dumpall \
+            -h localhost \
+            -U "${PSQLUSER}" \
+            --roles-only \
+    > roles.dev.dump
+
+podman run --rm \
+    --pod dev-media-pod \
     --name dev-media-pg-dump \
     --env "POSTGRES_PASSWORD_FILE=/secrets/psql-${PSQLUSER}" \
     --volume "/home/mmorano/maw/dev/media/data/pgpwd:/secrets" \
--- tools/dev-restore.sh
+++ tools/dev-restore.sh
@@ -10,8 +10,25 @@ podman run --rm \
     --volume "/home/mmorano/maw/dev/media/data/pgpwd:/secrets" \
     --volume "$(pwd):/input" \
     docker.io/aerisg222/maw-media-postgres:latest \
+        psql \
+            -h localhost \
+            -U "${PSQLUSER}" \
+            -X \
+            -f /input/roles.dev.dump
+
+podman run --rm \
+    --pod dev-media-pod \
+    --name dev-media-pg-load \
+    --security-opt label=disable \
+    --env "POSTGRES_PASSWORD_FILE=/secrets/psql-${PSQLUSER}" \
+    --volume "/home/mmorano/maw/dev/media/data/pgpwd:/secrets" \
+    --volume "$(pwd):/input" \
+    docker.io/aerisg222/maw-media-postgres:latest \
         pg_restore \
             -h localhost \
             -U "${PSQLUSER}" \
-            -d "${DB}" \
+            -d postgres \
+            --create \
+            --clean \
+            --if-exists \
             /input/maw_media.dev.dump
