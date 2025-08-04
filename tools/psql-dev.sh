cda56638a4dce5bcff85d6153619ed37f9ecfd07
Mike Morano <mmorano@mikeandwan.us>
2025-08-03 20:17:18 -0400

    updates for local changes

--- tools/psql-dev.sh
+++ tools/psql-dev.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-PSQLUSER=svc_maw_media
+PSQLUSER=postgres
 DB=maw_media
 
 podman run -it --rm \
     --pod dev-media-pod \
     --name dev-media-pg-query \
     --env "POSTGRES_PASSWORD_FILE=/secrets/psql-${PSQLUSER}" \
-    --volume "/home/mmorano/maw/dev/media/data/pgpwd:/secrets" \
+    --volume "/home/mmorano/maw-media/dev/pg-pwd:/secrets" \
     docker.io/library/postgres:17 \
         psql \
         -h localhost \
