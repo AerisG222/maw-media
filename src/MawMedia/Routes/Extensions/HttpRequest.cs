3741a4e80713516e1cf978029fd59add992d79f2
Mike Morano <mmorano@mikeandwan.us>
2025-08-08 07:07:38 -0400

    correct, otherwise it was including query string info

--- src/MawMedia/Routes/Extensions/HttpRequest.cs
+++ src/MawMedia/Routes/Extensions/HttpRequest.cs
@@ -1,11 +1,26 @@
-using Microsoft.AspNetCore.Http.Extensions;
-
 namespace MawMedia.Routes.Extensions;
 
 public static class HttpRequestExtensions
 {
-    public static string GetBaseUrl(this HttpRequest request) =>
-        request
-            .GetEncodedUrl()
-            .Replace(request.Path, string.Empty);
+    // credit: https://blog.elmah.io/how-to-get-base-url-in-asp-net-core/
+    public static string GetBaseUrl(this HttpRequest req)
+    {
+        if (req == null)
+        {
+            return string.Empty;
+        }
+
+        var uriBuilder = new UriBuilder(
+            req.Scheme,
+            req.Host.Host,
+            req.Host.Port ?? -1
+        );
+
+        if (uriBuilder.Uri.IsDefaultPort)
+        {
+            uriBuilder.Port = -1;
+        }
+
+        return uriBuilder.Uri.AbsoluteUri;
+    }
 }
